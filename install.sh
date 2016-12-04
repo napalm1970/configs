660f3072eb1032bb764801b10802124753ddfde5
napalm <napalm1970@gmail.com>
2016-12-04 20:25:42 +0200

    change install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,21 @@
+#! /bin/bash
 
-#!/bin/sh
-CONFIGDIR=$HOME/configs
 
-cp $HOME/configs/zsh/.zshrc $HOME/ 
-cp $HOME/configs/.Xresources $HOME/ 
+bash_conf_file=(.bash_aliases
+                .bash_history
+                .bashrc
+                .xinitrc
+                .Xresources)
+
+# myscripts 
+
+CONFIGDIR=./config
+
+
+# [ -e $CONFIGDIR ] || git clone https://github.com/napalm1970/configs config
+
+for file in ${bash_conf_file[*]}
+do
+    printf "    %s\n" $file
+done
 
