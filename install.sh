6c5279f03a5a215dce82ceeef7b0ab10f912630a
napalm <napalm1970@gmail.com>
2016-12-05 09:53:09 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -9,14 +9,13 @@ bash_conf_file=(.bash_aliases
 
 # myscripts 
 
-CONFIGDIR=$HOME/projects/bash/test
+CONFIGDIR=$HOME/config
 
 
-
-# [ -e $CONFIGDIR ] || git clone https://github.com/napalm1970/configs config
+[ -e $CONFIGDIR ] || git clone https://github.com/napalm1970/configs config
 
 for file in ${bash_conf_file[*]}
 do
-    ln -s `pwd`/$file $CONFIGDIR 
+    ln -s `pwd`/$file $HOME
 done
 
